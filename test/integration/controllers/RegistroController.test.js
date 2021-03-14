06df03b54af12a0b7f80a2c1b14f88c430828a87
Ernesto Duvalon <jorgeduvalon@nauta.cu>
2021-03-13 12:28:33 -1200

    Intento de prueba
    Se está trabajando en los test todavía

---
+++ test/integration/controllers/RegistroController.test.js
@@ -0,0 +1,15 @@
+/*var supertest = require('supertest');
+
+describe('RegistroController.create', function() {
+
+  describe('#create()', function() {
+    it('should redirect to /my/page', function (done) {
+      supertest(sails.hooks.http.app)
+      .post('/registro/create')
+      .send({ texto: 'test', evento: 'test' , fecha: 'test'})
+      .expect(302)
+      .expect('location','/my/page', done);
+    });
+  });
+
+});*/

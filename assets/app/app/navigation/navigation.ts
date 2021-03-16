17bc91413f77e668793572aba3cf6bb0b87cf8a2
NEO <neonavarro5@gmail.com>
2021-03-15 23:10:49 -0600

    interfaz sidebar

--- assets/app/app/navigation/navigation.ts
+++ assets/app/app/navigation/navigation.ts
@@ -3,8 +3,8 @@ import { FuseNavigation } from '../../@fuse/types';
 export const navigation: FuseNavigation[] = [
     {
         id       : 'applications',
-        title    : 'Applications',
-        translate: 'NAV.APPLICATIONS',
+        title    : 'Aplicaciones',
+        translate: 'Aplicaciones',
         type     : 'group',
         children : [
             {
@@ -24,116 +24,75 @@ export const navigation: FuseNavigation[] = [
         ]
     },
     {
-        id       : 'camping',
-        title    : 'Campings',
-        translate: 'NAV.CAMPINGS',
+        id       : 'campaña',
+        title    : 'Campañas',
+        translate: 'CAMPAÑA',
         type     : 'group',
         children : [
             {
-                id       : 'campings_table',
-                title    : 'Campings List',
-                translate: 'NAV.CampingsList.TITLE',
+                id       : 'buzon_opiniones',
+                title    : 'Buzon de Opiniones',
+                translate: 'Buzon de Opiniones',
                 type     : 'item',
                 icon     : 'list',
-                url      : '/#',
+                url      : '/apps/contacts',
                 badge    : {
                     title    : '25',
-                    translate: 'NAV.CampingsList.BADGE',
+                    translate: '25',
                     bg       : '#F44336',
                     fg       : '#FFFFFF'
                 }
-            }
+            },
+            {
+                id       : 'analisis_opiniones',
+                title    : 'Analisis de Opiniones',
+                translate: 'Analisis de Opiniones',
+                type     : 'item',
+                icon     : 'shopping_cart',
+                url      : '/apps/mail'
+            },
+        ]
+    },
+   
+    {
+        id       : 'estadisticas',
+        title    : 'Estadisticas',
+        translate: 'ESTADISTICAS',
+        type     : 'group',
+        children : [
+            {
+                id       : 'est_fecha',
+                title    : 'Estadisticas de opiniones por Fecha',
+                translate: 'Estadisticas por Fecha',
+                type     : 'item',
+                icon     : 'email',
+                url      : '/apps/dashboards/analytics'
+            },
+            {
+                 id      : 'est_idioma',
+                title    : 'Estadisticas de opiniones por Idiomas',
+                translate: 'Estadisticas por Idioma',
+                type     : 'item',
+                icon     : 'account_box',
+                url      : '/apps/dashboards/project'
+            },
         ]
     },
     {
-      id       : 'e-commerce',
-      title    : 'E-Commerce',
-      translate: 'NAV.ECOMMERCE',
-      type     : 'collapsable',
-      icon     : 'shopping_cart',
-      children : [
-          {
-              id        : 'products',
-              title     : 'Products',
-              type      : 'item',
-              url       : '/apps/e-commerce/products',
-              exactMatch: true
-          },
-          {
-              id        : 'productDetail',
-              title     : 'Product Detail',
-              type      : 'item',
-              url       : '/apps/e-commerce/products/1/printed-dress',
-              // exactMatch: true
-          },
-          {
-              id        : 'orders',
-              title     : 'Orders',
-              type      : 'item',
-              url       : '/apps/e-commerce/orders',
-              exactMatch: true
-          },
-          {
-              id        : 'orderDetail',
-              title     : 'Order Detail',
-              type      : 'item',
-              url       : '/apps/e-commerce/orders/1',
-              exactMatch: true
-          }
-      ]
-  },
-
-  {
-    id       : 'mail',
-    title    : 'Mail',
-    translate: 'NAV.MAIL.TITLE',
-    type     : 'item',
-    icon     : 'email',
-    url      : '/apps/mail',
-    badge    : {
-        title    : '25',
-        translate: 'NAV.MAIL.BADGE',
-        bg       : '#F44336',
-        fg       : '#FFFFFF'
+        id       : 'regist',
+        title    : 'Registro',
+        translate: 'REGISTRO',
+        type     : 'group',
+        children : [
+        {
+            id       : 'registro',
+            title    : 'Registro',
+            translate: 'Registro',
+            type     : 'item',
+            icon     : 'send',
+            url      : '/apps/todo'
+        },
+        ]
     }
-},
-// {
-//     id       : 'mail-ngrx',
-//     title    : 'Mail Ngrx',
-//     translate: 'NAV.MAIL_NGRX.TITLE',
-//     type     : 'item',
-//     icon     : 'email',
-//     url      : '/apps/mail-ngrx',
-//     badge    : {
-//         title    : '13',
-//         translate: 'NAV.MAIL_NGRX.BADGE',
-//         bg       : '#EC0C8E',
-//         fg       : '#FFFFFF'
-//     }
-// },
-{
-  id       : 'contacts',
-  title    : 'Contacts',
-  translate: 'NAV.CONTACTS',
-  type     : 'item',
-  icon     : 'account_box',
-  url      : '/apps/contacts'
-},
-{
-  id       : 'wizard',
-  title    : 'Wizard',
-  translate: 'NAV.WIZARD',
-  type     : 'item',
-  icon     : 'send',
-  url      : 'forms'
-},
-{
-  id   : 'profile',
-  title: 'Profile',
-  type : 'item',
-  icon : 'person',
-  url  : '/pages/profile'
-},
-
-
 ];
+

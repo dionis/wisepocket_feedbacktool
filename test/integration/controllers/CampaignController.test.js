4891725c64ba82ffbee886cb23f84e9348ffd084
Ernesto <jorgeduvalon@nauta.cu>
2021-03-26 18:57:39 -0400

    Update

--- test/integration/controllers/CampaignController.test.js
+++ test/integration/controllers/CampaignController.test.js
@@ -1,33 +1,24 @@
 var supertest = require('supertest');
 var faker = require('faker');
-var assert = require('assert');
+
 
 ///// SE INYECTA EN LA BASE DE DATOS AUTOMATICAMENTE
 //// SOLO SE REQUIERE SERVICIO DE MOSTRAR CAMPAÑAS POR USUARIO ACTIVO
 
-/*describe('CampaignController.getCampaignbyUser', function () {
+describe('CampaignController.getCampaignbyUser', function () {
     describe('#getCampaignbyUser', function () {
         it('should redirect to /my/page', function (done) {
-            Campaign.count({}).then(
-                beforeTest => {
-                    User.find({}).then(userList => {
-                        var user = null
-                        while (user == null) {
-                            user = faker.random.arrayElement(userList)
-                        }
-                    })
-               
             supertest(sails.hooks.http.app)
                 .get('/campaign/getCampaignbyUser/_id')
                 .query()
-                .send({ page: 0, id: userObjet.id })
+                .send({ page: 0, id: '605e4a4cd974b333bcbc8b4c' })
                 .expect(200, (err, res) => {
                     if (err) { return done(err); }
                     done();
                 });
-            });
+
         });
     })
-});*/
+});
 
 

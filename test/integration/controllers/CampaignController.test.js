a5647c44ddbd6159634e657e084f7d8a95dc3072
dionis <d.lopez@alltech.fr>
2021-03-27 07:05:58 -0400

    Fix messages

--- test/integration/controllers/CampaignController.test.js
+++ test/integration/controllers/CampaignController.test.js
@@ -15,10 +15,10 @@ describe('CampaignController.getCampaignbyUser', function () {
       // runs once after the last test in this block
     });
     describe('#getCampaignbyUser', function () {
-        it('should redirect to /my/page', async function (done) {
-            var zookeepers = await User.find();
+        it('should redirect to /my/page',  function (done) {
+           User.find().then(zookeepers=>{
             let user = zookeepers[0];
-            console.log("---- New Test execution data ----",  zookeepers[0])
+            console.log("---- New Test execution data ----",  zookeepers[0].id)
           // User.find().then(userResult => {
   ///https://www.npmjs.com/package/supertest
             console.log("FIND USER BY NAME " ,user.id);
@@ -27,15 +27,17 @@ describe('CampaignController.getCampaignbyUser', function () {
             .query({ page: 1, id: user.id })
             .expect(200)
             .then(response => {
+               ///EJEMPLO DE COMO VALIDAR LA RESPUESTA DEL SERVICIO
                // assert(response.body.email, 'foo@bar.com')
+                console.log("Test executed")
                 done();
             })
             .catch(err => done(err))
 
 
-        // }).catch(error =>{
-        //       throw Error(error)
-        // })
+        }).catch(error =>{
+              throw Error(error)
+        })
 
 
 

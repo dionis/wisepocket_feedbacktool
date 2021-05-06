a16b5b269d92e70b1fa300acfeeb80da06d4289e
Ernesto <jorgeduvalon@nauta.cu>
2021-05-06 12:06:50 -0400

    CReado el servicio de opiniones por idioma
    Opiniones paginadas por idioma en una campaña

--- test/integration/controllers/OpinionController.test.js
+++ test/integration/controllers/OpinionController.test.js
@@ -43,6 +43,44 @@ describe('OpinionController.getOpinion', function () {
   })
 });
 
+describe('OpinionController.getOpinionXIdiomaCamp', function () {
+  before(function () {
+    // runs once before the first test in this block
+    //https://mochajs.org/#hooks
+  });
+
+  after(function () {
+    // runs once after the last test in this block
+  });
+  describe('#getOpinionXIdiomaCamp', function () {
+    it('should redirect to /my/page', function (done) {
+      Campaign.find().then(zookeepers => {
+        let campaign = zookeepers[0];
+        console.log("---- New Test execution data ----", zookeepers[0].id)
+        // Campaign.find().then(campResult => {
+        ///https://www.npmjs.com/package/supertest
+        console.log("FIND Campaign BY NAME ", campaign.nombre);
+        supertest(sails.hooks.http.app)
+          .get('/opinion/getOpinionXIdiomaCamp').query({ page: 0, id: campaign.id, idioma: 'ingles'})
+          .expect(200)
+          .then(response => {
+            console.log("Service OK:" + response.text)
+             /// response.text >>>>> Lista de Opiniones por idioma en una campaña
+            done();
+          })
+          .catch(err => done(err))
+
+
+      }).catch(error => {
+        throw Error(error)
+      })
+
+
+
+    });
+  })
+});
+
 //DELETE TODAS LAS OPINIONES
 /*describe('OpinionController.deleteAllOpinion', function () {
     before(function() {
